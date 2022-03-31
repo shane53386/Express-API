3b79f01b0715fe2f15e1c1cb7d192ffba4d069fc
shane53386 <mchutipon@gmail.com>
2022-03-31 16:06:21 +0700

    Add security

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,6 @@ exports.register = async (req, res, next) => {
 	try {
 		const { name, email, password, role } = req.body;
 		const user = await User.create({ name, email, password, role });
-		// const token = user.getSignedJwtToken();
-		// res.status(200).json({ success: true, token });
 		sendTokenResponse(user, 200, res);
 	} catch (err) {
 		res.status(400).json({ success: false, data: err.message });
@@ -14,32 +12,38 @@ exports.register = async (req, res, next) => {
 };
 
 exports.login = async (req, res, next) => {
-	const { email, password } = req.body;
-	if (!email || !password) {
-		return res.status(400).json({
-			success: false,
-			msg: "Please provide an email and password",
-		});
-	}
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return res.status(400).json({
+				success: false,
+				msg: "Please provide an email and password",
+			});
+		}
 
-	const user = await User.findOne({ email }).select("+password");
-	if (!user) {
-		return res
-			.status(404)
-			.json({ success: false, msg: "Invalid credentials" });
-	}
+		const user = await User.findOne({ email }).select("+password");
 
-	const isMatch = await user.matchPassword(password);
-	if (!isMatch) {
+		if (!user) {
+			return res
+				.status(404)
+				.json({ success: false, msg: "Invalid credentials" });
+		}
+
+		const isMatch = await user.matchPassword(password);
+		if (!isMatch) {
+			return res.status(401).json({
+				success: false,
+				msg: "Invalid credentials",
+			});
+		}
+
+		sendTokenResponse(user, 200, res);
+	} catch (err) {
 		return res.status(401).json({
 			success: false,
-			msg: "Invalid credentials",
+			msg: "Cannot convert email or password to string",
 		});
 	}
-
-	// const token = user.getSignedJwtToken();
-	// res.status(200).json({ success: true, token });
-	sendTokenResponse(user, 200, res);
 };
 
 const sendTokenResponse = (user, statusCode, res) => {
@@ -63,8 +67,18 @@ const sendTokenResponse = (user, statusCode, res) => {
 
 exports.getMe = async (req, res, next) => {
 	const user = await User.findById(req.user.id);
+
 	res.status(200).json({
 		success: true,
 		data: user,
 	});
+};
+
+exports.logout = async (req, res, next) => {
+	res.cookie("token", "none", {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	res.status(200).json({ success: true, data: {} });
 };
